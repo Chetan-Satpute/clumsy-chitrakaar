f4c0bcd4a7009fb421b2ddd9f282ff041c3804cc
Chetan Satpute <chetan.satpute2002@gmail.com>
2024-11-23 19:20:51 +0530

    description, video and our products

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,10 +1,12 @@
 import Flicking from '@egjs/react-flicking';
 import logo from '~/assets/images/logo.png';
 import '@egjs/react-flicking/dist/flicking.css';
+import Flower from '~/assets/svgs/flower';
+import Item from './item';
 
 function Home() {
   return (
-    <div className="flex-1">
+    <div className="flex-1 overflow-auto">
       <div className="flex justify-center">
         <img src={logo} className="h-24" />
       </div>
@@ -26,6 +28,47 @@ function Home() {
           </div>
         </div>
       </Flicking>
+
+      <div className="my-10">
+        <p className="font-bold text-xl text-center mb-5">Handmade with Love</p>
+        <div className="bg-green-200 p-5 mx-5 lg:mx-10">
+          <p className="font-bold text-xs text-center">
+            Website & Products Overall Information Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </div>
+      </div>
+
+      <div className="relative mt-24 mb-10 bg-green-200 py-5 flex justify-end px-10">
+        <div className="absolute top-0 left-0 w-5/12 bg-white h-full z-0" />
+        <div className="absolute top-0 left-0 w-6/12 h-full z-20 flex justify-start items-center p-5">
+          <p className="font-extrabold text-xl">
+            Where art is more than just a vision.
+          </p>
+        </div>
+        <div className="z-20 absolute -top-11 right-5">
+          <Flower />
+        </div>
+        <div className="bg-blue-200 aspect-video w-5/6 rounded-md z-10"></div>
+      </div>
+
+      <div className="my-10 h-5 bg-green-300" />
+
+      <div className="mb-10">
+        <h4 className="text-xl text-center font-extrabold mb-5">Our Products</h4>
+
+        <div className="flex flex-wrap justify-around gap-5">
+          <Item />
+          <Item />
+          <Item />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/routes/home/item.tsx
@@ -0,0 +1,15 @@
+function Item() {
+  return (
+    <div>
+      <div className="p-5 bg-green-200">
+        <div className="aspect-square w-32 bg-orange-200 rounded-md"></div>
+      </div>
+      <div className="py-2">
+        <h6 className="font-extrabold text-lg text-center">Our Product</h6>
+        <p className="font-extrabold text-lg text-center">499/-</p>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
