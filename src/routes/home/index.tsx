d0ea14cf2d85656a5289dcb253a66e6d5dab4447
Chetan Satpute <chetan.satpute2002@gmail.com>
2024-11-23 17:36:22 +0530

    home horizontal carousel

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,4 +1,6 @@
+import Flicking from '@egjs/react-flicking';
 import logo from '~/assets/images/logo.png';
+import '@egjs/react-flicking/dist/flicking.css';
 
 function Home() {
   return (
@@ -6,6 +8,24 @@ function Home() {
       <div className="flex justify-center">
         <img src={logo} className="h-24" />
       </div>
+
+      <Flicking circular>
+        <div className="w-2/4 p-2">
+          <div className="aspect-square w-full bg-blue-200 flex justify-center items-center rounded-md">
+            1
+          </div>
+        </div>
+        <div className="w-2/4 p-2">
+          <div className="aspect-square w-full bg-green-200 flex justify-center items-center rounded-md">
+            2
+          </div>
+        </div>
+        <div className="w-2/4 p-2">
+          <div className="aspect-square w-full bg-orange-200 flex justify-center items-center rounded-md">
+            3
+          </div>
+        </div>
+      </Flicking>
     </div>
   );
 }
